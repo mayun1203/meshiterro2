c2f45948784fb0fb921f5371d547324fe90df88f
EC2 Default User <ec2-user@ip-172-31-34-9.ap-northeast-1.compute.internal>
2024-08-18 12:50:12 +0000

    マイページ編集設定

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,8 +4,8 @@ class PostImage < ApplicationRecord
 
   def get_image
     unless image.attached?
-      file_path = Rails.root.joi('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), folename: 'default-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: "homes#top"
   get "/homes/top", to: "homes#top"
 
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
 
   get "/homes/about", to: "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
